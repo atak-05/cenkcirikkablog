8257f6c6d60429024b4609f4aef52e382ae9a206
gizem <gizemtuncer17@gmail.com>
2023-03-10 16:15:47 +0300

    working on detail.html-category.html

--- blog/forms/contact.py
+++ blog/forms/contact.py
@@ -1,11 +1,16 @@
 from django import forms
 from blog.models.contact import contactModel
 from django.core.mail import send_mail
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 class contactForm(forms.ModelForm):
     class Meta:
         model = contactModel
         fields = ('name_lastname', 'email','message')
+    
+
+        
     def send_email(self,message):
         send_mail(
             subject='İletişim Formunda Yeni Mesaj Var',
